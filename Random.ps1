33092f622910222afab6b4863fd36678410e40d1
Tom Rune <trwakav@gmail.com>
2025-05-13 10:05:20 +0200

    First commit

---
+++ Random.ps1
@@ -0,0 +1,39 @@
+﻿Add-Type -AssemblyName System.Speech
+$SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+#$Path = "C:\Users\TomRuneWakaValen\Desktop\PowerShell Scripts\Out Of Context Ououts reader and fixer\Semi Filterd text.txt"       #This is for me
+
+$Path = ".\Semi Filterd text.txt"       #this is for frinds
+$lines = Get-Content -Path $Path
+for ($i = 1; $i -lt $lines.Count; $i++)
+{
+    $Safe = 2
+    $Random = Get-Random -Maximum $lines.Count
+    $line = $lines[$Random]
+    if(
+    $line -like  '*"*' -or
+    $line -like  '*“*' -or
+    $line -like  '*«*'
+    )
+    {
+        do
+        {
+            Write-Output "Well Fuck"
+            $Safe = $Safe + 1
+            $speak = $lines[$Random - $Safe]
+        }While (
+        $speak -like '*«*' -or
+        $speak -like '*“*' -or
+        $speak -like '*"*' -or
+        $speak -like " " -or
+        $speak -eq "" -or
+        $speak -match '^\d{1,2}/\d{1,2}/\d{4}' -or
+        $speak -match '^\d{1,2}:\d{2}' -or
+        $speak -match 'at \d{1,2}:\d{2} (AM|PM)'
+        )
+        $speak = $lines[$Random - $Safe]
+        $Quote = "$speak quoted: $line"
+        Write-Output $line
+        Write-Output $speak
+        $SpeechSynth.Speak($Quote)
+    }
+}
---
+++ Run_Cringe.bat
@@ -0,0 +1,1 @@
+powershell -ExecutionPolicy Bypass -File .\Random.ps1
